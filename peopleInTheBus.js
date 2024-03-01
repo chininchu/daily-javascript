8cc18d0c6abe1d74f25ec245cc18a4ca29e72bc9
Andrew Chu <andrew.c.chu@outlook.com>
2024-03-01 16:55:09 +0500

    Passed the peopleInTheBus test. Need to refactor the code

--- peopleInTheBus.js
+++ peopleInTheBus.js
@@ -1,1 +1,45 @@
 "use strict";
+
+let number = (busStops) => {
+
+
+    let totalOntheBus = 0;
+
+    let totalOftheBus = 0;
+
+
+    for (let i = 0; i < busStops.length; i++) {
+
+
+        for (let j = 0; j < 2; j++) {
+
+
+            if (j === 0) {
+
+                totalOntheBus += busStops[i][j];
+
+
+            } else if (j === 1) {
+
+
+                totalOftheBus += busStops[i][j];
+            }
+
+
+        }
+
+
+    }
+
+    return totalOntheBus - totalOftheBus;
+
+
+}
+
+
+console.log(number([[3, 0], [9, 1], [4, 8], [12, 2], [6, 1], [7, 8]]));
+
+
+
+
+
