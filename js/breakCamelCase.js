c3113e9977f99f7667e82a3c5c53288ab09018bf
Andrew Chu <andrew.c.chu@outlook.com>
2024-03-02 22:29:07 +0500

    breakCamelCase works. Need to be refactored

--- js/breakCamelCase.js
+++ js/breakCamelCase.js
@@ -5,8 +5,30 @@
 
 const solution = (string) => {
 
+    const splitArray = string.split("");
+
+    let result = [];
+
+    for (let i in splitArray) {
+
+        if (splitArray[i] === splitArray[i].toUpperCase()) {
+
+
+            result.push(splitArray[i] = " " + splitArray[i]);
+
+        } else if (splitArray[i] === splitArray[i].toLowerCase()) {
+
+            result.push(splitArray[i]);
+
+
+        }
+
+
+    }
+
+    return result.join((""));
+
 
 }
 
 
-console.log(solution("camelCasing"));
