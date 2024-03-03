2642b80c0843a0cbdd4bb70b864eb6b5f3a20d54
Andrew Chu <andrew.c.chu@outlook.com>
2024-03-03 15:07:36 +0500

    creditCardmark function is complete and ready for testing

---
+++ js/creditCardMask.js
@@ -0,0 +1,44 @@
+"use strict";
+
+
+/*Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'.*/
+
+
+const maskify = (cc) => {
+
+
+    const result = cc.split("").reduce((accum, currentValue, i, array) => {
+
+
+        let originalArray = array.length;
+
+        let finalArray = accum.length;
+
+
+        if (originalArray - finalArray <= 4) {
+
+
+            accum.push(currentValue);
+
+
+        } else {
+
+            accum.push("#");
+        }
+
+
+        return accum;
+
+
+    }, []);
+
+
+    return result.join("");
+
+
+}
+
+
+console.log(maskify("1234567890"));
