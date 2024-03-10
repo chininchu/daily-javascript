2c7987785da0e4fd9e288c68f7a7aa51b24e3568
Andrew Chu <andrew.c.chu@outlook.com>
2024-03-10 15:46:39 +0800

    Created a nested forLoop that checks for a blank string

--- js/simplePigLatin.js
+++ js/simplePigLatin.js
@@ -4,23 +4,24 @@
 Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.*/
 
 
-
-
-function pigIt(str){
+function pigIt(str) {
 
     let splitStr = str.split("");
 
+    for (let i = 0; i < splitStr.length; i++) {
+
+        for (let j = i + 1; j < splitStr.length; j++) {
+
+            if (splitStr[j].trim().length === 0) {
 
 
+            }
 
 
+        }
 
 
-
-
-
-
-
+    }
 
 
 }
